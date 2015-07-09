af6d555dc1d827ca6934c3b5b538b2558112d788
VortexLabs <logan@vortexlabs.net>
2015-07-08 20:13:17 -0700

    Initial Commit
    Testing Node

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express(); 
+var server = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+var port = process.env.ports || 3000;
+
+app.set('port', port); //Checks for available ports, default 3000
+
+server.listen(port, function(){
+	console.log('Port listening');
+});
+
+io.on('connection', function(socket){
+	console.log('New user connected');
+});
+
+
+
