653d949ff692a28b49a0da5f3f4d3b34d5644dfa
VortexLabs <logan@vortexlabs.net>
2015-07-08 21:45:53 -0700

    asdfasdfasdf
    asdfasdfasdfasdf

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var MongoClient = require('mongodb').MongoClient; 
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
   console.log('Got into app.get');
 });
 
-//app.use(express.static(__dirname + '/public'));
-
-//app.set('port', process.env.port || 3000); //Checks for available ports, default 3000
-
-//app.use(express.logger('dev'));
-
-/*if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}*/
-
-
 io.on('connection', function(socket){
 	console.log('New user connected');
 });
 
-
 http.listen(process.env.PORT || 5000, function(){
   console.log('listening on port' + process.env.PORT);
+});
+
+MongoClient.connect("mongodb://logan:macsaregay@ds047772.mongolab.com:47772/vortexlabs", function(err, db){
+	console.log('Everything is looking A-OK');
+	console.log(err);
 });
