009db0ed5330c4a29ef681adbecb6675f08c1756
VortexLabs <logan@vortexlabs.net>
2015-07-08 20:35:13 -0700

    Portttttt

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var consolidate = require('consolidate');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-var port = process.env.ports || 3000;
+var port = process.env.port || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-app.set('port', process.env.ports || 3000); //Checks for available ports, default 3000
+app.set('port', process.env.port || 3000); //Checks for available ports, default 3000
 
 server.listen(port, function(){
 	console.log('Port listening');
